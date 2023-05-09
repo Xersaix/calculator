0ec0ccbee99850a30a33a642d78f932bd8c44286
Xersaix <karl_kondamoyen@yahoo.fr>
2023-05-09 14:04:42 +0200

    Interface 2

---
+++ assets/js/calculator.js
@@ -0,0 +1,16 @@
+
+// Creation et initialisation des variables
+var button_input = document.querySelectorAll(".button");
+
+
+// Ajout d'event listener
+
+button_input[0].addEventListener("click",function(){
+    console.log("7");
+})
+button_input[1].addEventListener("click",function(){
+    console.log("8");
+})
+button_input[2].addEventListener("click",function(){
+    console.log("9");
+})
